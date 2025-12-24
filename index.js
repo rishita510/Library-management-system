314dc17b9959b31508aeeaa8ef861cdf14fac854
rishita2410 <isroscience2@gmail.com>
2025-12-24 20:06:32 +0530

    added different request method

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express= require('express');
+const user= require("./data/user.json");
 const app = express();
 const port=8081;
 
@@ -9,9 +10,74 @@ app.get('/', (req, res) => {
     message: 'Welcome to Library Management System'
    })
 })
-app.all('*',(req,res) =>{
-    res.status(500).json({
-        message: 'not built yet'
+app.get('/user',(req,res) =>{
+    res.status(200).json({
+        success: true,
+        data: user
+    })
+})
+
+app.get('/user/:id',(req,res) =>{
+    const id= req.params.id;
+    const person= user.find((each) => each.id===Number(id));
+
+    if(!user){
+        return res.status(404).json({
+            success:false,
+            message: "Person not found"
+        })
+    }
+
+  res.status(200).json({
+    success:true,
+    data: person
+  })
+})
+
+app.post('/user',(req,res) =>{
+    // "id": 4,
+    // "name": "David Smith",
+    // "email": "dsmith88@example.com",
+    // "role": "Premium Member",
+    // "purchased_books": [],
+    // "account_status": "Active"
+    const {id,name,email,role,purchased_books,account_status}= req.body;
+
+    if(!id || !name || !email || !role || !purchased_books || !account_status){
+        return res.status(400).json({
+            success:false,
+            message: "Please provide complete details"
+        })
+    }
+    const newuser=user.find((each) => each.id===Number(id));
+    if(newuser){
+        return res.status(400).json({
+            success:false,
+            message: "User with this id already exists"
+        })
+    }
+    user.push({id,name,email,role,purchased_books,account_status});
+    res.status(201).json({
+        success:true,
+        data: user
+    })
+})
+app.put('/user/:id',(req,res) =>{
+    const id= req.params.id;
+    const {name,email,role,purchased_books,account_status}= req.body;
+    const person= user.find((each) => each.id===Number(id));
+
+    if(!person){
+        return res.status(404).json({
+            success:false,
+            message: "Person not found"
+        })
+    }
+    const index= user.findIndex((each) => each.id===Number(id));
+    user[index]={id,name,email,role,purchased_books,account_status};
+    res.status(200).json({
+        success:true,
+        data: user
     })
 })
 
